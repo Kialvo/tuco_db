02f4c7d7e7083c5e9c1e0e6d7db46c83baf14081
MarvinDinaku <marvindinaku@gmal.com>
2025-02-21 13:21:04 +0100

    amazing commit

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Contact;
 use Illuminate\Http\Request;
+use function PHPUnit\Framework\isNull;
 
 class ContactsController extends Controller
 {
@@ -58,7 +59,7 @@ class ContactsController extends Controller
             // Add the Action column (Edit/Delete buttons).
             ->addColumn('action', function ($contact) {
                 // If this row is soft-deleted, we only show a “Restore” button
-                if ($contact->trashed()) {
+                if (!isNull($contact->deleted_at)) {
                     $restoreUrl = route('contacts.restore', $contact->id);
                     return '
                     <form action="'.$restoreUrl.'" method="POST" style="display:inline;">
