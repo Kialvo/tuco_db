9bc85a51ea03667e54db8ffb657cf4e97ebb3b84
MarvinDinaku <marvindinaku@gmal.com>
2025-02-24 16:26:04 +0100

    amazing commit

--- resources/views/websites/index.blade.php
+++ resources/views/websites/index.blade.php
@@ -101,7 +101,6 @@
             <button id="btnSearch" class="px-4 py-2 bg-blue-600 text-white rounded">Search</button>
             <button id="btnClear" class="px-4 py-2 bg-gray-600 text-white rounded ml-2">Clear Filters</button>
         </div>
-        </div>
 
     </div>
 
