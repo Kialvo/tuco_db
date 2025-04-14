d524cffa4bf09f0c9f8a7cf5dd36395f0f2f6e93
MarvinDinaku <marvindinaku@gmal.com>
2025-04-14 13:05:31 +0200

    amazing commit

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,6 +7,11 @@
 
         <title>{{ config('app.name', 'Linkinablink') }}</title>
 
+        <!-- Favicon -->
+        <link rel="icon" type="image/x-icon" href="{{ asset('favicon.png') }}">
+
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
